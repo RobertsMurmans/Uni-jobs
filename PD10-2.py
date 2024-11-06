bf4ea37648fe77cf616c4dbcb5955310a1fd8cf3
RobertsMurmans <112924947+RobertsMurmans@users.noreply.github.com>
2024-11-06 15:55:47 +0200

    Create PD10-2.py

---
+++ PD10-2.py
@@ -0,0 +1,10 @@
+virkne = input("Ievade >> ")
+
+garums = len(virkne)
+
+for x in range(garums):
+    for i in range(x, x+garums):
+        index = i % garums
+        print(virkne[index],end="")
+    print()
+    
