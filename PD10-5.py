8dade8e6fec264dfcf1ae424aff58462cc1ac811
RobertsMurmans <112924947+RobertsMurmans@users.noreply.github.com>
2024-11-06 16:53:52 +0200

    Update PD10-5.py

--- PD10-5.py
+++ PD10-5.py
@@ -2,10 +2,14 @@ from decimal import *
 getcontext().prec = 5
 
 r = Decimal(input("Radius: "))
+summa = 0
 
 if r < Decimal(2).sqrt():
-    print("Rinkis par mazu lai butu pat viens!")
+    print("Rinkis par mazu!")
     exit()
 
 for x in range(1,int(r.quantize(Decimal("1."), rounding=ROUND_UP))):
-    print(x)
+    y = r * Decimal(1-(Decimal(x/r)**2)).sqrt()
+    summa += int(y.quantize(Decimal("1."), rounding=ROUND_DOWN))
+
+print(summa*4) 
