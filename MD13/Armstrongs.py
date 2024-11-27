69ced978322a2a5b3ce00de1a306eb6a92bf828e
RobertsMurmans <112924947+RobertsMurmans@users.noreply.github.com>
2024-11-27 13:18:39 +0200

    lookup table
    lookup table is op, expecially as the number of digits increases, got it down to about a minute at 8 digits

--- MD13/Armstrongs.py
+++ MD13/Armstrongs.py
@@ -1,44 +1,73 @@
+def tabula(cipars):
+    match cipars:
+        case "0":
+            return 0
+        case "1":
+            return 1
+        case "2":
+            return divi
+        case "3":
+            return triss
+        case "4":
+            return cetri
+        case "5":
+            return pieci
+        case "6":
+            return sesi
+        case "7":
+            return septini
+        case "8":
+            return astoni
+        case "9":
+            return devini
+    return 0
+
+
 def pakape(baze, kapinatajs):
     rezultats = 1
     for n in range(kapinatajs):
         rezultats = rezultats*baze
     return rezultats
+    
 
-
-def summa(skaitlis, cipari):
+def summa(skaitlis):
     summa = 0
-
     for cipars in str(skaitlis):
-        summa = summa + pakape(int(cipars), cipari)
+        summa = summa + tabula(cipars)
         if summa > skaitlis:
             return 0
     return summa == skaitlis
-
-
-def main():
-    armstrongs = ""
-    cipari = 5
-
-    while True:
-        maksimums = pakape(10, cipari)
-        minimums = pakape(10, cipari-1)
-
-        x = minimums
-        while x < maksimums:
-            if summa(x, cipari):
-                armstrongs = str(x)
-            x += 1
-      
-        print(f"Aprekinats {cipari} ciparu gars skaitlis, lielakais lidz sim atrastais armstronga skaitlis ir: {armstrongs}.")
-
-        if input("Turpinat(y/n)? ")[0] == "y":
-            cipari += 1
-        else: 
-            break
-
-    print(f"Lielakais atrastais armstronga skaitlis ir: {armstrongs}")
     
 
-if __name__ == "__main__":
-    main()
+#=======================================================================#
 
+armstrongs = ""
+cipari = 5
+
+while True:
+    divi = pakape(2,cipari)
+    triss = pakape(3,cipari)
+    cetri = pakape(4,cipari)
+    pieci = pakape(5,cipari)
+    sesi = pakape(6,cipari)
+    septini = pakape(7,cipari)
+    astoni = pakape(8,cipari)
+    devini = pakape(9,cipari)
+
+    maksimums = pakape(10, cipari)
+    minimums = pakape(10, cipari-1)
+
+    x = minimums
+    while x < maksimums:
+        if summa(x):
+            armstrongs = str(x)
+        x += 1
+     
+    print(f"Aprekinats {cipari} ciparu gars skaitlis, lielakais lidz sim atrastais armstronga skaitlis ir: {armstrongs}.")
+
+    if input("Turpinat(y/n)? ")[0] == "y":
+        cipari += 1
+    else: 
+        break
+
+print(f"Lielakais atrastais armstronga skaitlis ir: {armstrongs}")
