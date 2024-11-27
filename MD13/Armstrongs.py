8df8b15beeed2c44b5fccbfda191a4d0c2e1a373
RobertsMurmans <112924947+RobertsMurmans@users.noreply.github.com>
2024-11-27 12:00:03 +0200

    simple optimization
    Made it so that when a sum is too big or too small to be able to be equal to the sum, it moves to the next number

--- MD13/Armstrongs.py
+++ MD13/Armstrongs.py
@@ -4,11 +4,18 @@ def pakape(baze, kapinatajs):
         rezultats = rezultats*baze
     return rezultats
 
+
 def summa(skaitlis, cipari):
     summa = 0
+    deviniVertiba = pakape(9,cipari)
+    maxSumma = deviniVertiba*cipari
+    
     for cipars in str(skaitlis):
-        summa = summa+pakape(int(cipars),cipari)
-    return summa
+        summa = summa + pakape(int(cipars), cipari)
+        maxSumma = maxSumma + maxSumma - deviniVertiba
+        if summa > skaitlis > maxSumma:
+            return 0
+    return summa == skaitlis
 
 
 def main():
@@ -16,23 +23,25 @@ def main():
     cipari = 3
 
     while True:
-        maksimums = pakape(10,cipari)
+        maksimums = pakape(10, cipari)
         minimums = pakape(10, cipari-1)
 
-        for x in range(minimums,maksimums):
-            if summa(x, cipari) == x:
+        x = minimums
+        while x < maksimums:
+            if summa(x, cipari):
                 armstrongs = str(x)
+            x += 1
         
-        print(f"Aprekinats {cipari} ciparu gars skaitlis, lielakais, lidz sim atrastais armstronga skaitlis ir: {armstrongs}")
+        print(f"Aprekinats {cipari} ciparu gars skaitlis, lielakais lidz sim atrastais armstronga skaitlis ir: {armstrongs}.")
 
         if input("Turpinat(y/n)?")[0] == "y":
             cipari += 1
         else: 
             break
 
-    print(armstrongs)
-
+    print(f"Lielakais atrastais armstronga skaitlis ir: {armstrongs}")
     
+
 if __name__ == "__main__":
     main()
 
