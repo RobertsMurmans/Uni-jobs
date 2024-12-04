24307d07e0132740b346723dea9f127cca9f3b37
RobertsMurmans <112924947+RobertsMurmans@users.noreply.github.com>
2024-12-04 08:46:15 +0200

    Complete re write
    changed the loop at the base of the function and now it is incomparably faster

--- MD13/Armstrongs.py
+++ MD13/Armstrongs.py
@@ -1,74 +1,50 @@
-def tabula(cipars):
-    match cipars:
-        case "0":
-            return 0
-        case "1":
-            return 1
-        case "2":
-            return divi
-        case "3":
-            return triss
-        case "4":
-            return cetri
-        case "5":
-            return pieci
-        case "6":
-            return sesi
-        case "7":
-            return septini
-        case "8":
-            return astoni
-        case "9":
-            return devini
-        case "m":
-            return mx
-    return -1
-
-
-def pakape(baze, kapinatajs):
-    rezultats = 1
-    for n in range(kapinatajs):
-        rezultats = rezultats*baze
-    return rezultats
+def kapinat(baze, kapinatajs):
+    if kapinatajs == 0:
+        return 1
+    elif kapinatajs < 0:
+        return 1 / kapinat(baze, -kapinatajs)
+    elif kapinatajs % 2 == 0:
+        pus_kapinatajs = kapinat(baze, kapinatajs // 2)
+        return pus_kapinatajs * pus_kapinatajs  
+    else:
+        return baze * kapinat(baze, kapinatajs - 1)
     
+armstrongs = 0
 
-def summa(skaitlis):
-    summa = 0
-    maxSum = tabula("m")
-    for cipars in str(skaitlis):
-        summa = summa + tabula(cipars)
-        maxSum = maxSum - tabula("9") + tabula(cipars)
-        if summa > skaitlis or skaitlis > maxSum:
-            return 0
-    return 1
+def sort(string):
+    rezult = string[0]
+    for element in string[1:]:
+        for i in range(len(rezult)):
+            if int(element) > int(rezult[i]):
+                rezult = rezult[0:i] + element + rezult[i:]
+                break
+        else:
+            rezult = rezult + element
+    return rezult
 
-#=======================================
-armstrongs = ""
-cipari = 5
+
+def cikls(start, index, summa, cipars, pakape):
+    if index == 0:
+        if sort(str(summa)) == cipars:
+            global armstrongs
+            if armstrongs < summa:
+                armstrongs = summa
+            return 
+        return 
+    for i in range(start, -1, -1):
+        cikls(i, index-1, summa+kapinat(i,pakape), cipars+str(i), pakape)
+
+digits = 7
 
 while True:
-    x = pakape(10, cipari)
-    minimums = pakape(10, cipari-1)
-    divi = pakape(2,cipari)
-    triss = pakape(3,cipari)
-    cetri = divi*divi
-    pieci = pakape(5,cipari)
-    sesi = triss*divi
-    septini = pakape(7,cipari)
-    astoni = cetri*divi
-    devini = triss*triss
-    mx = devini*cipari
-    while x > minimums:
-        if summa(x):
-            armstrongs = str(x)
-            break
-        x -= 1
 
-    print(f"Aprekinats {cipari} ciparu gars skaitlis, lielakais lidz sim atrastais armstronga skaitlis ir: {armstrongs}.")
-
-    if input("Turpinat(y/n)? ")[0] == "y":
-        cipari += 1
-    else: 
+    for i in range(9,0,-1):
+        cikls(i,digits-1,kapinat(i,digits),str(i), digits)
+    
+    print(f"{digits} digit number calulated, largest one found so far is: {armstrongs}.")
+    if (input("Do you want to continue? ")[0] != "y"):
         break
+    digits+=1
 
-print(f"Lielakais atrastais armstronga skaitlis ir: {armstrongs}")
+print(armstrongs)
+
